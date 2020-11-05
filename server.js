8252a4216e7143a7a428ba2032058a1b46322a82
Petrossonbygg <69109575+Petrossonbygg@users.noreply.github.com>
2020-11-05 13:54:44 +0200

    hover

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
-
 const express = require('express')
-const socketIO = require('socket.io')
-const app = express()
 
 const server = express()
-    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-
-
-const io = socketIO(server);
-
-const users = {}
-
-io.on("connection", socket => {
-    socket.on('new-user', name => {
-        users[socket.id] = name
-        socket.broadcast.emit('user-connected', name)
+    .use(function (req, res) {
+        console.log('Time: %d', Date.now())
+        //res.sendfile(__dirname + '/index.html');
     })
-    socket.on('send-chat-message', message => {
-        socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] });
-    })
-    socket.on('disconnect', () => {
-        socket.broadcast.emit('user-disconnected', users[socket.id]);
-        delete users[socket.id]
-    })
-});
+    // .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+    .listen(PORT, () => {
+        console.log('listening on port 3000')
+        const socketIO = require('socket.io')
+        const io = socketIO(server);
+
+        const users = {}
+
+        io.on("connection", socket => {
+            socket.on('new-user', name => {
+                users[socket.id] = name
+                socket.broadcast.emit('user-connected', name)
+            })
+            socket.on('send-chat-message', message => {
+                socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] });
+            })
+            socket.on('disconnect', () => {
+                socket.broadcast.emit('user-disconnected', users[socket.id]);
+                delete users[socket.id]
+            })
+        });
+    });
